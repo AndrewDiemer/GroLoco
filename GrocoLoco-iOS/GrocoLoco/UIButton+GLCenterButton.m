3959304ae9ea9f69903fc283dca2ab9c7392d06d
Mark Hall <golfguru@icloud.com>
2015-11-16 00:17:40 -0500

    did the stupid fucking categories view

---
+++ GrocoLoco-iOS/GrocoLoco/UIButton+GLCenterButton.m
@@ -0,0 +1,35 @@
+//
+//  UIButton+HDYLCenterButton.m
+//  
+//
+//  Created by Mark Hall on 2015-07-31.
+//
+//
+
+#import "UIButton+GLCenterButton.h"
+
+@implementation UIButton (GLCenterButton)
+
+- (void)centerImageAndTitle:(float)spacing
+{
+    // get the size of the elements here for readability
+    CGSize imageSize = self.imageView.frame.size;
+    CGSize titleSize = self.titleLabel.frame.size;
+    
+    // get the height they will take up as a unit
+    CGFloat totalHeight = (imageSize.height + titleSize.height + spacing);
+    
+    // raise the image and push it right to center it
+    self.imageEdgeInsets = UIEdgeInsetsMake(- (totalHeight - imageSize.height), 0.0, 0.0, - titleSize.width);
+    
+    // lower the text and push it left to center it
+    self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (totalHeight - titleSize.height),0.0);
+}
+
+- (void)centerImageAndTitle
+{
+    const int DEFAULT_SPACING = 6.0f;
+    [self centerImageAndTitle:DEFAULT_SPACING];
+}
+
+@end
