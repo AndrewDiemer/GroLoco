6ea7641fad3b04b3276dff3f97ea42baccdf6e73
Mark Hall <golfguru@icloud.com>
2016-03-06 17:26:03 -0500

    sale items show in categories now

---
+++ GrocoLoco-iOS/GrocoLoco/GLPromotionView.h
@@ -0,0 +1,16 @@
+//
+//  GLPromotionView.h
+//  GrocoLoco
+//
+//  Created by Mark Hall on 2016-03-06.
+//  Copyright © 2016 Mark Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class GLGroceryItem;
+
+@interface GLPromotionView : UIView
+
+@property (nonatomic, strong) GLGroceryItem *item;
+
+@end
