924428d74d1e8aa50b8e6d71b10acf32d09e79ab
Mark Hall <golfguru@icloud.com>
2016-03-26 12:12:26 -0400

    fixed login error, added images to buttons

--- GrocoLoco-iOS/GrocoLoco/GLMapViewController.m
+++ GrocoLoco-iOS/GrocoLoco/GLMapViewController.m
@@ -54,7 +54,7 @@
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
-    self.navigationController.navigationBarHidden = NO;
+//    self.navigationController.navigationBarHidden = NO;
 }
 
 #pragma mark -
