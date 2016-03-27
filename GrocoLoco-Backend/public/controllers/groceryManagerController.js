0aac1e1d7d5a6b872a6ba71fdda1c384ca1a35b0
wghozayel <wghozayel@gmail.com>
2016-03-27 17:41:07 -0400

    Hype tings

--- GrocoLoco-Backend/public/controllers/groceryManagerController.js
+++ GrocoLoco-Backend/public/controllers/groceryManagerController.js
@@ -1,4 +1,50 @@
 
 GrocoLoco.controller('groceryManagerController', function($scope, $http, $location) {
 
+	$scope.createGroceryItem = function(){
+
+		$http.get('/groceries').success(function(data, status){
+
+			for( var i = 0; i<data.length; i++ ){
+				if($scope.Description == data[i].Description){
+					$scope.Description = "An item already exists with this name.";
+					return;
+				}
+				else if($scope.Price == null){
+					$scope.Description = "A valid price must be entered.";
+					return;
+				}
+				else if($scope.Category == null){
+					$scope.Description = "A category must be entered.";
+					return;
+				}
+			}
+
+			var item = {
+				Category 	: $('#category').val(),
+				Price		: $scope.Price,   
+				Description : $scope.Description, 
+				IconLink 	: $scope.IconLink
+			}
+				
+			$http.post("/createGroceryItem", item).success(function(data,status){
+			
+				$scope.Price = ""; 
+				$scope.Description = "";
+				$scope.IconLink = "";
+				$scope.Category = "";
+
+				console.log(data);
+
+
+			}).error(function(data, status){
+			  	//something went wrong
+			  	console.log(status)
+			  	console.log(data)
+			})
+
+		})
+			
+	
+	}
 })
